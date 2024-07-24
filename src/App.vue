74fa3e6ea2ad0da007cf1922c2629e935af0289c
Emirhan Ilıca <89273703+ilicaemirhan@users.noreply.github.com>
2024-07-24 10:58:12 +0300

    Completed CLI Basics

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
 <template>
   <h1>{{ title }}</h1>
-  <input type="text" ref="name">
-  <button @click="handleClick()">Click me</button>
-  <ModalComponent ref="modalcomponent"/>
+  <p>Welcome...</p>
+  <teleport to=".modals" v-if="showModal">
+      <ModalComponent ref="modalcomponent" theme="sale" @close="toggleModal()">
+        <template v-slot:links>
+          <a href="#">Sign Up NOW!</a>
+          <a href="#">For More Info</a>
+        </template>
+        <h1>FREE CAT PICS! 🐈</h1>
+        <p>Sign up now and get free cat pics delivered to your inbox every day!</p>
+      </ModalComponent>
+  </teleport>
+
+  <teleport to=".modals" v-if="showModalDog">
+    <ModalComponent @close="toggleDogModal">
+        <template v-slot:links>
+          <!-- <a href="#">Why are we still here?</a>
+          <a href="#">Just to suffer?</a> -->
+        </template>
+        <h1>FREE DOG PICS! 🐶</h1>
+        <p>Sign up now and get free dog pics delivered to your inbox every day!</p>
+      </ModalComponent>
+  </teleport>
+  <button @click="toggleModal">Show Cat Modal</button>
+  <button @click="toggleDogModal">Show Dog Modal</button>
 </template>
 
 <script>
 import ModalComponent from './components/ModalComponent.vue'
+
 export default {
   name: 'App',
   components: {
@@ -14,21 +36,26 @@ export default {
   },
   data(){
     return{
-      title: "Hello Vue"
+      title: "Hello Vue",
+      header: "Sign Up for Free Cat Pics 🐈",
+      text: "Don't you miss out 😎",
+      showModal: false,
+      showModalDog: false
     }
   },
   methods: {
-    handleClick(){
-      console.log(this.$refs.name)
-      this.$refs.name.classList.add('active')
-      this.$refs.name.focus()
+    toggleModal(){
+      this.showModal = !this.showModal
+    },
+    toggleDogModal(){
+      this.showModalDog = !this.showModalDog
     }
   }
 }
 </script>
 
 <style>
-#app {
+#app, .modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
